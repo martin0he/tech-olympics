eae124cfd89747d86fb654dd8ff1afee8e25203d
eashapatel <132864058+eashapatel@users.noreply.github.com>
2025-03-30 14:09:17 -0400

    Create solution.py

---
+++ challenge-11/solution.py
@@ -0,0 +1,35 @@
+class TransactionParser:
+
+  def __init__():
+
+    self.transactions = [{"Name":"Erika", "CustomerID": "C001", "TransactionAmount":120.00},
+                         {"Name":"Amelia", "CustomerID": "C002", "TransactionAmount":75.00}, 
+                         {"Name":"Jules", "CustomerID": "C003", "TransactionAmount":180.50}, 
+                         {"Name":"Erika", "CustomerID": "C001", "TransactionAmount":90.00}, 
+                         {"Name":"Quinn", "CustomerID": "C004", "TransactionAmount":55.00},
+                         {"Name":"Amelia", "CustomerID": "C005", "TransactionAmount"50.00},
+                         {"Name":"Frani", "CustomerID": "C003", "TransactionAmount":200.00}, 
+                         {"Name":"Jules", "CustomerID": "C003", "TransactionAmount":50.00}, 
+                         {"Name":"Henry", "CustomerID": "C006", "TransactionAmount":250.00}, 
+                         {"Name":"Cookies", "CustomerID": "C007", "TransactionAmount":300.00},
+                         {"Name":"Erika", "CustomerID": "C001", "TransactionAmount":110.00},
+                         {"Name":"Jules", "CustomerID": "C003", "TransactionAmount":220.00}, 
+                         {"Name":"Frani", "CustomerID": "C005", "TransactionAmount":125.00}, 
+                         {"Name":"Quinn", "CustomerID": "C004", "TransactionAmount":95.00}, 
+                         {"Name":"Amelia", "CustomerID": "C002", "TransactionAmount":250.00},
+                         {"Name":"Easha", "CustomerID": "C015", "TransactionAmount":55.00},
+                         {"Name":"Ayla", "CustomerID": "C009", "TransactionAmount":90.00}, 
+                         {"Name":"Will", "CustomerID": "C017", "TransactionAmount":30.00}, 
+                         {"Name":"Lorelei", "CustomerID": "C018", "TransactionAmount":25.00}, 
+                         {"Name":"Kevin", "CustomerID": "C019", "TransactionAmount":75.00},
+                         {"Name":"Owen", "CustomerID": "C020", "TransactionAmount":50.00}, 
+                         {"Name":"Liz", "CustomerID": "C021", "TransactionAmount":80.00}]
+                         
+
+
+   
+
+                         
+
+                         
+                         
