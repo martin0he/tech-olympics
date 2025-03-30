85695ce1555f2ca59e6eb56ac65299471de2d6fb
Jhonyel Galvis <147948058+Jhonyel@users.noreply.github.com>
2025-03-30 14:14:37 -0400

    answer challenge 14

---
+++ challenge-14/answer.py
@@ -0,0 +1,11 @@
+def fibonacci(i):
+    if (i <= 1):
+        return i
+   else:
+    return fibonacci(i - 1) + fibonacci(i - 2)
+
+for i in range (10000000):
+    sum = 0
+    if (fibonacci(i) % 2 != 0):
+        sum += fibonacci(i)
+    return sum
